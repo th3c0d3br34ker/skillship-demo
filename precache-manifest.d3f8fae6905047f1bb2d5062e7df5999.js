303527a2a5972e3e2c960eb8aa2c1dc08ab54ffc
th3c0d3br34ker <jainamvipul.desai2019@vitstudent.ac.in>
2020-09-08 14:18:07 +0530

    Updates

---
+++ precache-manifest.d3f8fae6905047f1bb2d5062e7df5999.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16175dd2294f8e871e5033b19204880",
+    "url": "/skillship-demo/index.html"
+  },
+  {
+    "revision": "04c485221f46bab5a474",
+    "url": "/skillship-demo/static/css/main.c50d8d90.chunk.css"
+  },
+  {
+    "revision": "f980798c94234b6078d4",
+    "url": "/skillship-demo/static/js/2.1cf44d1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/skillship-demo/static/js/2.1cf44d1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04c485221f46bab5a474",
+    "url": "/skillship-demo/static/js/main.c28382eb.chunk.js"
+  },
+  {
+    "revision": "c871ad63d685e77d7c55",
+    "url": "/skillship-demo/static/js/runtime-main.7baebe38.js"
+  }
+]);
